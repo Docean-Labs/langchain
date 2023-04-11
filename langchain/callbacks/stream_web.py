098802a6e9a4fd3bb4c602947e2d0c171609a84c
tanshaokang <2528855098@qq.com>
2023-04-11 17:45:05 +0800

    fix: multi-fianl answer

--- langchain/callbacks/stream_web.py
+++ langchain/callbacks/stream_web.py
@@ -13,58 +13,58 @@ class StreamingWebCallbackHandler(BaseCallbackHandler):
         super().__init__(*args, **kwargs)
         self._callback = callback
 
-    def on_llm_start(
+    async def on_llm_start(
             self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
         """Run when LLM starts running."""
 
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
         if self._callback:
-            self._callback(token)
+            await self._callback(token)
         else:
             sys.stdout.write(token)
             sys.stdout.flush()
 
-    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when LLM ends running."""
 
-    def on_llm_error(
+    async def on_llm_error(
             self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> None:
         """Run when LLM errors."""
 
-    def on_chain_start(
+    async def on_chain_start(
             self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
     ) -> None:
         """Run when chain starts running."""
 
-    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
+    async def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Run when chain ends running."""
 
-    def on_chain_error(
+    async def on_chain_error(
             self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> None:
         """Run when chain errors."""
 
-    def on_tool_start(
+    async def on_tool_start(
             self, serialized: Dict[str, Any], input_str: str, **kwargs: Any
     ) -> None:
         """Run when tool starts running."""
 
-    def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
+    async def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
         """Run on agent action."""
         pass
 
-    def on_tool_end(self, output: str, **kwargs: Any) -> None:
+    async def on_tool_end(self, output: str, **kwargs: Any) -> None:
         """Run when tool ends running."""
 
-    def on_tool_error(
+    async def on_tool_error(
             self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> None:
         """Run when tool errors."""
 
-    def on_text(self, text: str, **kwargs: Any) -> None:
+    async def on_text(self, text: str, **kwargs: Any) -> None:
         """Run on arbitrary text."""
 
-    def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> None:
+    async def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> None:
         """Run on agent end."""
