2b8c201345a4151ff06950c975db5d1fed33597b
tanshaokang <2528855098@qq.com>
2023-05-20 18:57:37 +0800

    feat: no write callback

--- langchain/callbacks/stream_web.py
+++ langchain/callbacks/stream_web.py
@@ -1,13 +1,18 @@
 """Callback Handler streams to stdout on new llm token."""
 import sys
 from typing import Any, Dict, List, Union, Optional, Callable
+from uuid import UUID
 
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.schema import AgentAction, AgentFinish, LLMResult
+from langchain.schema import AgentAction, AgentFinish, LLMResult, BaseMessage
 
 
 class StreamingWebCallbackHandler(BaseCallbackHandler):
     """Callback handler for streaming. Only works with LLMs that support streaming."""
+
+    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], *, run_id: UUID,
+                            parent_run_id: Optional[UUID] = None, **kwargs: Any) -> Any:
+        pass
 
     def __init__(self, callback: Optional[Callable] = None, billing: Optional[Callable] = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,6 +30,76 @@ class StreamingWebCallbackHandler(BaseCallbackHandler):
         else:
             sys.stdout.write(token)
             sys.stdout.flush()
+
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        """Run when LLM ends running."""
+
+    async def on_llm_error(
+            self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        """Run when LLM errors."""
+
+    async def on_chain_start(
+            self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
+    ) -> None:
+        """Run when chain starts running."""
+
+    async def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
+        """Run when chain ends running."""
+
+    async def on_chain_error(
+            self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        """Run when chain errors."""
+
+    async def on_tool_start(
+            self, serialized: Dict[str, Any], input_str: str, **kwargs: Any
+    ) -> None:
+        """Run when tool starts running."""
+
+    async def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
+        """Run on agent action."""
+        pass
+
+    async def on_tool_end(self, output: str, **kwargs: Any) -> None:
+        """Run when tool ends running."""
+
+    async def on_tool_error(
+            self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        """Run when tool errors."""
+
+    async def on_text(self, text: str, **kwargs: Any) -> None:
+        """Run on arbitrary text."""
+
+    async def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> None:
+        """Run on agent end."""
+
+    async def on_billing_action(self, prompt_tokens: int, completion_tokens: int, **kwargs: Any) -> None:
+        """on billing action"""
+        if self._billing:
+            await self._billing(prompt_tokens, completion_tokens)
+
+
+class StreamingWebNoWriteCallbackHandler(BaseCallbackHandler):
+    """Callback handler for streaming. Only works with LLMs that support streaming."""
+
+    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], *, run_id: UUID,
+                            parent_run_id: Optional[UUID] = None, **kwargs: Any) -> Any:
+        pass
+
+    def __init__(self, callback: Optional[Callable] = None, billing: Optional[Callable] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._callback = callback
+        self._billing = billing
+
+    async def on_llm_start(
+            self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> None:
+        """Run when LLM starts running."""
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        pass
 
     async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when LLM ends running."""
